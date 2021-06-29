6be25ebc9534cd3839f17f127aef27a2b00f7b93
Michelle Li <michelleyli123@gmail.com>
2021-06-28 21:31:52 -0400

    added another project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,6 +4,7 @@ import ProjectCard from './ProjectCard';
 
 import othello from "../../assets/projects/othello.PNG";
 import paint from "../../assets/projects/paint.PNG";
+import random from "../../assets/projects/random.PNG"
 
 function Projects(){
     return (
@@ -29,8 +30,17 @@ function Projects(){
                         <ProjectCard
                         img={paint}
                         title="Network Paint"
-                        description="Used Java, GUIs, and Network Programming in order to create a painting program that allows clients to connect onto the same server and paint on the same canvas using unique colors"
+                        description="Used Java, GUIs, and socket programming in order to create a painting program that allows clients to connect onto the same server and paint on the same canvas using unique colors"
                         link="https://github.com/michelleli01/network-paint"
+                        />
+                    </Col>
+
+                    <Col md={4} className="project-card">
+                        <ProjectCard
+                        img={random}
+                        title="Random Sampling App"
+                        description="Used Java and Android Studio to develop an Android app that allows the user to randomly input their emotions throughout the day. The app will randomly notify the user at random times of the day to fill out the survey and will then save the user's data to the devices sd card."
+                        link="https://github.com/michelleli01/random-sampling-app"
                         />
                     </Col>
                 </Row>
