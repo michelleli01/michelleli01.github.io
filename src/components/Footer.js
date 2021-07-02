c795f078a495cd293e62494d1ff407f68624d1e4
Michelle Li <michelleyli123@gmail.com>
2021-07-02 18:09:36 -0400

    finished footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,9 @@ function Footer(){
         <Container fluid className="footer">
             <Row>
                 <Col md="4" className="footer-copyright">
-                    <h3>Designed and Developed by Michelle Li </h3>
+                    <h3>Designed and Developed by Michelle Li</h3>
+                    <h3>Built from scratch with React, JavaScript, HTML5, and CSS3</h3>
+                    
                 </Col>
                 
                 <Col md="4" className="footer-body">
@@ -17,6 +19,11 @@ function Footer(){
                         <a href="https://github.com/michelleli01"><FiGithub/></a>
                         <a href="https://www.linkedin.com/in/michelle-li-0342951a9/"><FaLinkedin/></a>
                     </div>
+                </Col>
+
+                <Col md="4" className="footer-contact">
+                    <h3>301-250-0346</h3>
+                    <h3>michelleyli123@gmail.com</h3>
                 </Col>
             </Row>
         </Container>
